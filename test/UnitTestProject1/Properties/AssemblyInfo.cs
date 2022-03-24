76457a9e1675719229e1bc5803e30850778b1573
dotnet <sobinasir.84@gmail.com>
2022-03-24 12:49:37 +0530

    add unit test
    Add unit test

--- test/UnitTestProject1/Properties/AssemblyInfo.cs
+++ test/UnitTestProject1/Properties/AssemblyInfo.cs
@@ -2,18 +2,18 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("test")]
+[assembly: AssemblyTitle("UnitTestProject1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("test")]
+[assembly: AssemblyProduct("UnitTestProject1")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
-[assembly: Guid("8c257e1f-5f0d-44fd-b29e-62ea4aa9e00a")]
+[assembly: Guid("d76c8699-c718-4054-8684-1e54277e1f79")]
 
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
