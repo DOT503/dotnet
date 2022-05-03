ceab70da812a3624046d6889a34193a998a84d39
dotnet <sobinasir.84@gmail.com>
2022-05-03 12:31:18 +0530

    update publish.bat

--- build/publish.bat
+++ build/publish.bat
@@ -1,4 +1,22 @@
 
 set version=%1.%2.%3
-github-release release --user DOT503 --repo dotnet --tag v%version% --name v%version% 
-github-release upload --user DOT503 --repo dotnet --tag v%version% --name "bank.exe" --file "%cd%\..\src\bank\bin\Debug\bank.exe" --replace
+github-release release --user DOT503 --repo dotnet --tag v%version% --name v%version%
+echo "wait"
+echo "wait"
+echo "wait"
+echo "wait"
+echo "wait"
+echo "wait"
+echo "wait"
+echo "wait"
+echo "wait"
+echo "wait"
+echo "wait"
+echo "wait"
+echo "wait"
+echo "wait"
+echo "wait"
+echo "wait"
+echo "wait"
+echo "wait"
+github-release upload --user DOT503 --repo dotnet --tag v%version% --name "bank.exe" --file "%cd%\..\src\bank\bank\bin\Debug\bank.exe" --replace
