b3b538e0e7c33dd2f618d1563ddab28d857b98ab
dotnet <sobinasir.84@gmail.com>
2022-05-03 08:32:38 +0530

    Update Build.bat
    fix build issue

--- build/Build.bat
+++ build/Build.bat
@@ -1,9 +1,9 @@
 set vs_version=2019
 set vs_type=Community
-set build_type="Debug"
+set build_type=Debug
 REM start building the solution
 
-"C:\Program Files (x86)\Microsoft Visual Studio\%vs_version%\%vs_type%\Common7\IDE\devenv.exe" "%cd%\..\src\bank\bank.sln" /rebuild "%build_type%"
+"C:\Program Files (x86)\Microsoft Visual Studio\%vs_version%\%vs_type%\Common7\IDE\devenv.exe" "%cd%\..\src\bank\bank.sln" /build "%build_type%"
 
 REM start building the unit test
 "C:\Program Files (x86)\Microsoft Visual Studio\%vs_version%\%vs_type%\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe" "%cd%\..\test\UnitTestProject1\bin\%build_type%\UnitTestProject1.dll
